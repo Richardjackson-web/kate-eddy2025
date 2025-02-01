c8d3b36c06a1c69a2cf75458513f725d958d524b
Richardjackson-web <richardjackson556@gmail.com>
2025-02-01 07:33:26 +0100

    Added some more sections

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function(e) {
       e.preventDefault();
